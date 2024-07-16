dfcd79e91ca42a26d8f51f64d3ef5661e8f9c6e6
Saul <saulcordovavill@gmail.com>
2024-07-16 17:24:01 -0400

    Se creo una funcion que verifica si es necesario un salto de linea

---
+++ app/src/main/java/com/spokefan/finalpokedex/presentation/pokemonDetails/PokemonDetailsScreen.kt
@@ -0,0 +1,12 @@
+package com.spokefan.finalpokedex.presentation.pokemonDetails
+
+private fun addLineBreakToCategory(category: String): String {
+    val words = category.split(" ")
+    val wordCount = words.size
+    return if (wordCount <= 2) {
+        category.replaceFirst(" ", "\n")
+    } else {
+        val index = category.lastIndexOf(" ")
+        category.substring(0, index) + "\n" + category.substring(index + 1)
+    }
+}
