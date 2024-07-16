a0dd95b1faf40aa9ef37f44b61a62add6f0a2c00
Saul <saulcordovavill@gmail.com>
2024-07-16 17:49:09 -0400

    Se creo lo faltante para el diseño de la presentacion del pokemon

--- app/src/main/java/com/spokefan/finalpokedex/presentation/pokemonDetails/PokemonDetailsScreen.kt
+++ app/src/main/java/com/spokefan/finalpokedex/presentation/pokemonDetails/PokemonDetailsScreen.kt
@@ -1,9 +1,16 @@
 package com.spokefan.finalpokedex.presentation.pokemonDetails
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -12,12 +19,91 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.spokefan.finalpokedex.domain.model.PokemonDetails
 import com.pokedex.R
 import com.spokefan.finalpokedex.ui.theme.interFont
 
 
+@Composable
+private fun PokemonDetailsRow(
+    pokemonDetails: PokemonDetails
+) {
+    Row(
+        modifier = Modifier
+            .padding(horizontal = 10.dp)
+            .padding(top = 20.dp)
+            .fillMaxWidth()
+            .height(80.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceEvenly
+    ) {
+        Text(
+            text = addLineBreakToCategory(pokemonDetails.category),
+            color = Color.Black,
+            fontFamily = interFont,
+            fontWeight = FontWeight.Medium,
+            fontSize = 17.sp,
+            textAlign = TextAlign.Center
+        )
+
+        Box(modifier = Modifier
+            .width(2.5.dp)
+            .height(65.dp)
+            .background(Color(0x4FD3D3D3))
+        )
+
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(3.dp)
+        ) {
+            Text(
+                text = "${pokemonDetails.weight} lbs.",
+                color = Color.Black,
+                fontFamily = interFont,
+                fontWeight = FontWeight.Medium,
+                fontSize = 17.sp
+            )
+
+            Text(
+                text = "Weight",
+                color = Color(0xFF7E7E7E),
+                fontFamily = interFont,
+                fontWeight = FontWeight.Normal,
+                fontSize = 16.sp
+            )
+        }
+
+        Box(modifier = Modifier
+            .width(2.5.dp)
+            .height(65.dp)
+            .background(Color(0x4FD3D3D3))
+        )
+
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(3.dp)
+        ) {
+            Text(
+                text = "${pokemonDetails.height}'",
+                color = Color.Black,
+                fontFamily = interFont,
+                fontWeight = FontWeight.Medium,
+                fontSize = 17.sp
+            )
+
+            Text(
+                text = "Height",
+                color = Color(0xFF7E7E7E),
+                fontFamily = interFont,
+                fontWeight = FontWeight.Normal,
+                fontSize = 16.sp
+            )
+        }
+    }
+}
 @Composable
 fun PokemonDescriptionSection(
     flavorText: String
