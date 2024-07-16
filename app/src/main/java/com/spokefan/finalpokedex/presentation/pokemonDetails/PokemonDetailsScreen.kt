f145b92a8359192e62a1a4dc10032bd9c38c7852
Saul <saulcordovavill@gmail.com>
2024-07-16 17:30:15 -0400

    se añadio una funcion que da una fuente y un color a la descripcion del pokemon

--- app/src/main/java/com/spokefan/finalpokedex/presentation/pokemonDetails/PokemonDetailsScreen.kt
+++ app/src/main/java/com/spokefan/finalpokedex/presentation/pokemonDetails/PokemonDetailsScreen.kt
@@ -1,5 +1,51 @@
 package com.spokefan.finalpokedex.presentation.pokemonDetails
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.pokedex.R
+
+
+@Composable
+fun PokemonDescriptionSection(
+    flavorText: String
+) {
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .padding(horizontal = 19.dp, vertical = 16.dp)
+    ) {
+        val description = flavorText.replace("\n", " ")
+
+        Text(
+            text = description,
+            color = Color(0xFF7E7E7E),
+            fontFamily = interFont,
+            fontWeight = FontWeight.Normal,
+            fontSize = 16.sp
+        )
+
+        Icon(
+            painter = painterResource(id = R.drawable.down_arrow),
+            contentDescription = null,
+            tint = Color(0xEEA5A5A5),
+            modifier = Modifier
+                .size(33.dp)
+                .align(Alignment.BottomCenter)
+        )
+    }
+}
+
 private fun addLineBreakToCategory(category: String): String {
     val words = category.split(" ")
     val wordCount = words.size
